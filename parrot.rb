6b2656de15847e0583b919ca789912714f5afde9
Nick Ruggieri <nicolasruggieri7@gmail.com>
2019-05-08 15:52:43 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(string)
-  puts "#{string}"
-  return string
   puts "Squak!"
   return "Squak!"
+  puts "#{string}"
+  return string
 end
